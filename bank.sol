9ad31fe52c72eac9a6bd7c03385b4e9bbd552879
travis8gunter <travisjgunter@gmail.com>
2022-01-31 00:23:29 -0700

    first sol code

---
+++ bank.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.8.7;
+
+contract Bank {
+    mapping(address => uint256) public accounts;
+
+    modifier hasFunds(uint256 _amount) {
+        require(_amount <= accounts[msg.sender], "Does not own sufficient funds.");
+        _;
+    }
+    function deposit() public payable {
+        accounts[msg.sender] += msg.value;
+    }
+    function withdraw(uint256 _amount) public hasFunds(_amount) {
+        payable(msg.sender).transfer(_amount);
+    }
+    function checkAssets() public view returns(uint256){
+        return address(this).balance;
+    }
+}
